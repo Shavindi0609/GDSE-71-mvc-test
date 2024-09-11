2d056eef706763484e886a198554510ab99aa5e0
Shavindi0609 <shavindialoka69@gmail.com>
2024-09-11 02:06:03 -0700

    Update CustomerController.java

--- src/mvc/test/MVCTest/edu/ijse/controller/CustomerController.java
+++ src/mvc/test/MVCTest/edu/ijse/controller/CustomerController.java
@@ -4,10 +4,41 @@
  */
 package mvc.test.MVCTest.edu.ijse.controller;
 
+import mvc.test.MVCTest.edu.ijse.dto.CustomerDTO;
+import mvc.test.MVCTest.edu.ijse.model.CustomerModel;
+import java.util.List;
+
 /**
  *
  * @author Shavindi
  */
 public class CustomerController {
+    private CustomerModel customerModel = new CustomerModel();
+
+    public String saveCustomer(CustomerDTO customerDTO) throws Exception {
+        String resp = customerModel.saveCustomer(customerDTO);
+        return resp;
+    }
+
+    public String updateCustomer(CustomerDTO customerDTO) throws Exception {
+        String resp = customerModel.updateCustomer(customerDTO);
+        return resp;
+    }
+
+    public String deleteCustomer(String customerId) throws Exception {
+        String resp = customerModel.deleteCustomer(customerId);
+        return resp;
+    }
     
+    public CustomerDTO searchCustomer(String customerId) throws Exception{
+        CustomerDTO dto = customerModel.searchCustomer(customerId);
+        return dto;
+    }
+    
+    public List<CustomerDTO> getAllCustomer() throws Exception{
+        List<CustomerDTO> dtos = customerModel.getAllCustomer();
+        return dtos;
+    }
+
+}
 }
